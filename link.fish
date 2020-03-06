b6d3a468b7cf7aad0347f7bb3489734e15762bcf
Shumpei IINUMA <shumpei.iinuma@gmail.com>
2020-03-06 23:19:41 +0900

    fix(link.sh): ignore .gitignore, etc.

--- link.fish
+++ link.fish
@@ -1,4 +1,4 @@
 set -q HOME
 
 find . -type d -name '.git' -prune -o -type d -print0 | xargs -0 -L 1 -I {} mkdir -pv $HOME/{}
-find . -type d -name '.git' -prune -o -type f -print0 | xargs -0 -L 1 -I {} ln -f -s (pwd)/{} $HOME/{}
+find . -type d -name '.git' -prune -o -type f -not -name '.gitignore' -not -name 'README.md' -not -name 'link.fish' -print0 | xargs -0 -L 1 -I {} ln -v -f -s (pwd)/{} $HOME/{}
