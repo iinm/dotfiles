e9c93cdf5398a9c09ff797fedcce2aee05c36f43
Shumpei IINUMA <shumpei.iinuma@gmail.com>
2020-05-04 21:33:17 +0900

    chore(fish): remove unused config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,27 +1,6 @@
 set -x LANG en_US.UTF-8
 set -x EDITOR nvim
-
 set -x PATH ~/tools/bin $PATH
-
-# https://adoptopenjdk.net/releases.html
-if test -L ~/tools/java
-  set -x PATH ~/tools/java/bin $PATH
-end
-
-# https://golang.org/dl/
-if test -L ~/tools/golang
-  set -x PATH ~/tools/golang/bin $PATH
-end
-
-# https://nodejs.org/ja/download/
-if test -L ~/tools/nodejs
-  set -x PATH ~/tools/nodejs/bin $PATH
-end
-
-# https://docs.docker.com/engine/security/rootless/
-if test -S "/run/user/"(id -u)"/docker.sock"
-  set -x DOCKER_HOST "unix:///run/user/"(id -u)"/docker.sock"
-end
 
 if test (uname) = 'Linux'
   alias pbcopy  'xsel -i -p && xsel -o -p | xsel -i -b'
@@ -30,7 +9,7 @@ if test (uname) = 'Linux'
 end
 
 # Host specific configuration
-if type -q local_config
+if type --quiet local_config
   local_config
 end
 
@@ -53,14 +32,14 @@ if status --is-interactive
   alias view 'nvim -R'
   alias dco  'docker-compose'
 
-  if type -q fzf
+  if type --quiet fzf
     set -x FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow --exclude .git --exclude "*~"'
     set -x FZF_DEFAULT_OPTS    '--reverse'
     set -x FZF_CTRL_T_COMMAND  $FZF_DEFAULT_COMMAND
     set -x FZF_CTRL_T_OPTS     $FZF_DEFAULT_OPTS
   end
 
-  if type -q fzf_key_bindings
+  if type --quiet fzf_key_bindings
     fzf_key_bindings
   end
 end
