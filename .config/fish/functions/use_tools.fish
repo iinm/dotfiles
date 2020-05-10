35ae8c9514421d26ea492b78b36d326528b936f7
Shumpei IINUMA <shumpei.iinuma@gmail.com>
2020-05-10 22:47:31 +0900

    refactor(fish): find bin dir

--- .config/fish/functions/use_tools.fish
+++ .config/fish/functions/use_tools.fish
@@ -18,11 +18,11 @@ function use_tools
   function tools_default_path --description "Show default path"
     echo $TOOLS/bin
     for link in (find $TOOLS -mindepth 1 -maxdepth 1 -type l)
-      __tools_bin_path $link
+      __tools_bin_dir $link
     end
   end
 
-  function default --argument-names name_pattern --description "Set default version by creating symlink"
+  function default --argument-names name_pattern --description "Set default version"
     # $TOOLS/foo-1.0
     set version_dir (find $TOOLS -mindepth 1 -maxdepth 1 -type d | grep "$name_pattern" | fzf)
     # $TOOLS/foo
@@ -39,30 +39,36 @@ function use_tools
     # $TOOLS/foo-1.0/bin
     set path (
       for tool_dir in (find $TOOLS -mindepth 1 -maxdepth 1 -type d)
-        __tools_bin_path $tool_dir
+        __tools_bin_dir $tool_dir
       end \
         | grep "$name_pattern" \
         | fzf
     )
-    # $TOOLS/foo-1.0/bin -> $TOOLS/foo-1.0
-    set tool_dir (echo "$path" | sed -E 's,(.+)/bin,\1,')
-    # $TOOLS/foo-1.0 -> $TOOLS/foo
-    set path_pattern \
-      (dirname "$tool_dir")/(basename "$tool_dir" | __tools_trim_version)
-    set -gx PATH "$path" (echo $PATH | tr ' ' '\n' | grep -vE "^$path_pattern")
+    set -gx PATH "$path" $PATH
   end
 
-  function __tools_bin_path --argument-names tool_dir --description "Show path for a tool directory"
+  function __tools_bin_dir --argument-names tool_dir --description "Find and show bin directory"
     if test -d $tool_dir/bin
       echo $tool_dir/bin
-    else if test -d $tool_dir/Contents/Home/bin # JDK
-      echo $tool_dir/Contents/Home/bin
-    else
-      echo $tool_dir
+      return
     end
+
+    set bin_dir (
+      find $tool_dir/ -maxdepth 3 -type d -name 'bin' \
+        | awk '{ print length, $0 }' \
+        | sort -n \
+        | awk '{ print $2 }' \
+        | head -1 | sed 's,//,/,g'
+    )
+    if test -n "$bin_dir"
+      echo $bin_dir
+      return
+    end
+
+    echo $tool_dir
   end
 
-  function __tools_trim_version --argument-names name_with_version --description "Trim version e.g. foo-1.0 -> foo"
+  function __tools_trim_version --argument-names name_with_version --description "Trim version string"
     sed -E 's/^([a-zA-Z-]+[a-zA-Z]).*/\1/' \
       | sed -E 's/(.+)-(latest|v)/\1/'
   end
