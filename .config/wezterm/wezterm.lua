1c635a31a8e62bdf3cf6c0f5950fbddc9fb09fcd
Shumpei IINUMA <shumpei.iinuma@gmail.com>
2022-06-23 10:15:45 +0900

    chore(wezterm): change font size

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm';
 
 return {
   font = wezterm.font("Operator Mono SSm", { weight="Book" }),
-  font_size = 13,
+  font_size = 12.5,
   color_scheme = "Chalk",
   use_ime = true,
   hide_tab_bar_if_only_one_tab = true,
