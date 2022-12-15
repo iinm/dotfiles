feb1178c4272d71d9f10e1d9c9301412151f314a
Shumpei IINUMA <shumpei.iinuma@gmail.com>
2022-12-15 10:41:08 +0900

    feat(wezterm): set fallback font, change color

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,9 +1,12 @@
 local wezterm = require 'wezterm';
 
 return {
-  font = wezterm.font("Operator Mono SSm", { weight="Book" }),
+  font = wezterm.font_with_fallback {
+    { family = "Operator Mono SSm", weight = "Book" },
+    { family = "Hiragino Sans" },
+  },
   font_size = 12.5,
-  color_scheme = "Chalk",
+  color_scheme = "Eighties (base16)",
   use_ime = true,
   hide_tab_bar_if_only_one_tab = true,
   adjust_window_size_when_changing_font_size = false,
