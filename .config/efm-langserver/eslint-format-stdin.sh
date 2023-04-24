582176c8198924c751fc6e382aef024e08f3f008
Shumpei IINUMA <shumpei.iinuma@gmail.com>
2023-04-24 22:35:10 +0900

    fix: eslint format script

--- .config/efm-langserver/eslint-format-stdin.sh
+++ .config/efm-langserver/eslint-format-stdin.sh
@@ -4,5 +4,8 @@ set -euo pipefail
 
 filename="${1?}"
 
-(npx --no-install eslint -f json --stdin --fix-dry-run --stdin-filename "$filename" || true) \
+test -e ./node_modules/.bin/eslint
+
+# https://eslint.org/docs/latest/use/command-line-interface#exit-codes
+(npx --no-install eslint -f json --stdin --fix-dry-run --stdin-filename "$filename" || test "$?" -lt 2) \
   | jq -e -r '.[0].output | rtrimstr("\n")'
