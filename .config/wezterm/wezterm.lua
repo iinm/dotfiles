769afe10bac099a9371c9960de3f7472f44fd50f
Shumpei IINUMA <shumpei.iinuma@gmail.com>
2023-05-09 21:04:06 +0900

    update wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,13 +1,24 @@
 local wezterm = require 'wezterm';
+local local_config = require 'local_config';
 
-return {
-  font = wezterm.font_with_fallback {
-    { family = "Operator Mono SSm", weight = "Book" },
-    { family = "Hiragino Sans" },
-  },
-  font_size = 12.5,
-  color_scheme = "Eighties (base16)",
-  use_ime = true,
-  hide_tab_bar_if_only_one_tab = true,
-  adjust_window_size_when_changing_font_size = false,
+local config = {}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.font = wezterm.font_with_fallback {
+  { family = "Operator Mono SSm", weight = "Book" },
+  { family = "Hiragino Sans" },
+  { family = "Noto Sans CJK JP" },
 }
+config.font_size = 12.5
+config.color_scheme = "Eighties (base16)"
+config.use_ime = true
+config.hide_tab_bar_if_only_one_tab = true
+config.adjust_window_size_when_changing_font_size = false
+
+-- host specific config
+local_config.apply_to_config(config)
+
+return config
