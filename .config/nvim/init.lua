da22e4fdb615d155d8ab6b95b4d37349f23a421c
Shumpei IINUMA <shumpei.iinuma@gmail.com>
2023-07-28 17:37:22 +0900

    feat(nvim): use lazy

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -329,72 +329,66 @@ local setup_auto_commands = function()
 end
 
 local ensure_plugins = function()
-  -- https://github.com/wbthomason/packer.nvim#bootstrapping
-  local ensure_packer = function()
-    local fn = vim.fn
-    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-    if fn.empty(fn.glob(install_path)) > 0 then
-      fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
-      vim.cmd [[packadd packer.nvim]]
-      return true
-    end
-    return false
+  -- https://github.com/folke/lazy.nvim
+  local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+  if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+      'git',
+      'clone',
+      '--filter=blob:none',
+      'https://github.com/folke/lazy.nvim.git',
+      '--branch=stable', -- latest stable release
+      lazypath,
+    })
   end
+  vim.opt.rtp:prepend(lazypath)
 
-  local packer_bootstrap = ensure_packer()
-
-  require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'
-
+  require('lazy').setup({
     -- ui
-    use 'sainnhe/everforest'
+    'sainnhe/everforest',
+    'stevearc/dressing.nvim',
 
     -- utilities
-    use 'ctrlpvim/ctrlp.vim'
-    use 'tpope/vim-sleuth'
-    use 'tpope/vim-commentary'
-    use 'tpope/vim-fugitive'
-    use 'elihunter173/dirbuf.nvim'
-    use 'akinsho/toggleterm.nvim'
-    use 'windwp/nvim-autopairs'
-    use 'kamykn/spelunker.vim'
-    use 'brenoprata10/nvim-highlight-colors'
-    use 'kylechui/nvim-surround'
-    use 'stevearc/dressing.nvim'
-    use { 'phaazon/hop.nvim', branch = 'v2' }
+    'ctrlpvim/ctrlp.vim',
+    'tpope/vim-sleuth',
+    'tpope/vim-commentary',
+    'tpope/vim-fugitive',
+    'elihunter173/dirbuf.nvim',
+    'akinsho/toggleterm.nvim',
+    'windwp/nvim-autopairs',
+    'kamykn/spelunker.vim',
+    'brenoprata10/nvim-highlight-colors',
+    'kylechui/nvim-surround',
+    { 'phaazon/hop.nvim', branch = 'v2' },
 
     -- lsp
-    use 'neovim/nvim-lspconfig'
+    'neovim/nvim-lspconfig',
 
     -- debugger
-    use 'mfussenegger/nvim-dap'
-    use 'rcarriga/nvim-dap-ui'
+    'mfussenegger/nvim-dap',
+    'rcarriga/nvim-dap-ui',
 
     -- snippets
-    use 'hrsh7th/vim-vsnip'
-    use 'rafamadriz/friendly-snippets'
+    'hrsh7th/vim-vsnip',
+    'rafamadriz/friendly-snippets',
 
     -- completion
-    use 'hrsh7th/cmp-buffer'
-    use 'hrsh7th/cmp-vsnip'
-    use 'hrsh7th/cmp-nvim-lsp'
-    use 'hrsh7th/cmp-path'
-    use 'hrsh7th/cmp-cmdline'
-    use 'hrsh7th/nvim-cmp'
-    use 'github/copilot.vim'
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-vsnip',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-cmdline',
+    'hrsh7th/nvim-cmp',
+    'github/copilot.vim',
 
     -- languages
-    use 'pangloss/vim-javascript'
-    use 'jose-elias-alvarez/typescript.nvim'
-    use 'jparise/vim-graphql'
-    use 'hashivim/vim-terraform'
-    use 'dag/vim-fish'
-    use 'digitaltoad/vim-pug'
-
-    if packer_bootstrap then
-      require('packer').sync()
-    end
-  end)
+    'dag/vim-fish',
+    'pangloss/vim-javascript',
+    'jose-elias-alvarez/typescript.nvim',
+    'jparise/vim-graphql',
+    'hashivim/vim-terraform',
+    'digitaltoad/vim-pug',
+  })
 end
 
 local setup_toggleterm = function()
