d1b6b1ef23168c765368ee0ec86afb348676a4ed
Shumpei IINUMA <shumpei.iinuma@gmail.com>
2024-04-21 19:25:48 +0900

    chore(nvim): add cheat sheet

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,67 @@
+--[[ Cheat Sheet
+start                vim -u NONE (does not load init.lua)
+open file            :e **/foo.sh
+                     :e %:h/ (open directory of current file)
+                     :sp foo.sh (split)
+                     :vsp foo.sh (vsplit)
+motions              } (next paragraph)
+                     { (previous paragraph)
+                     C-f (next page)
+                     C-b (previous page)
+scroll               zz (center)
+                     zt (top)
+                     zb (bottom)
+search               /foo -> n (next) -> N (previous)
+                     * (next <cword>)
+                     % (previous <cword>)
+command              :foo
+                     :execute "normal! ..." (execute normal mode command)
+                       - `execute` will handle special characters like `<CR>`
+                       - `normal!` ignores mappings
+                     :foo <C-r>" (paste from register)
+replace              :%s/foo/bar/g
+                     :%s/<C-r>/bar/g (replace last search)
+marks                :marks
+                     ma (set mark a)
+                     `a (jump to mark a)
+                     `` (jump to previous mark)
+                     :delmarks a (delete mark a)
+                     :delmarks! a (delete all marks)
+jumps                :jumps -> [N] Ctrl-o (older location) or Ctrl-i (newer location)
+recent files         :browse oldfiles
+                     :browse filter /foo.*/ oldfiles
+grep                 :grep! foo -> :cw
+                     :grep! foo % (current buffer)
+                     :grep! <cword> (cursor word)
+                     :grep! \b<cword>\b
+close buffers        :bd foo* -> Ctrl-a (close all matched)
+                     :%bd (close all) -> C-o (back to previous buffer)
+window               C-w C-w (next window)
+                     C-w -> o (close other windows)
+                     C-w -> = (equalize window size)
+                     C-w -> _ (maximize height)
+                     C-w -> | (maximize width)
+                     C-w -> c (close)
+netrw                :e .
+                     :e . -> i -> i -> i (tree view)
+                     :e . -> p (preview)
+                     :e . -> d (make directory)
+                     :e . -> % (new file)
+                     :e . -> D (delete)
+                     :e . -> R (rename)
+                     :e . -> mt (mark target) -> mf (markfile) -> mm (move)
+                     :e . -> mt (mark target) -> mf (markfile) -> mc (copy)
+                     :e . -> mu (unmark all)
+quickfix             :cw(indow) (open quickfix window)
+                     :colder (older quickfix list)
+                     :cnewer (newer quickfix list)
+                     :cdo %s/foo/bar/g (replace in quickfix list)
+open path            gf (goto file)
+                     gx (xdg-open)
+                     C-w gf (open in new tab)
+folding              zc (close), zo (open), zO (open all)
+--]]
+
 local require_safe = function(name)
   local ok, module = pcall(require, name)
   if not ok then
