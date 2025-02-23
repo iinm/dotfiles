9048e8fcb73499f237c8d6beeb0295c022da52b1
Shumpei IINUMA <shumpei.iinuma@gmail.com>
2025-02-23 12:36:04 +0900

    refactor: rename agent to interpreter

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -19,7 +19,7 @@ fish_add_path $HOME/tools/go/bin
 fish_add_path $HOME/tools/lua-language-server/bin
 fish_add_path $HOME/tools/google-cloud-sdk/bin
 
-fish_add_path $HOME/dotfiles/agent/bin
+fish_add_path $HOME/dotfiles/interpreter/bin
 
 # Environment variables
 set -x SHELL (which fish)
