732421ba26b70756637cc5d5dd829479dfb9c4ca
Shumpei IINUMA <shumpei.iinuma@gmail.com>
2025-03-17 21:52:02 +0900

    feat(agent): switch implementation

--- agent/env.sh
+++ agent/env.sh
@@ -14,18 +14,9 @@ if test -f "$this_dir/.secrets/anthropic-api-key.txt"; then
   export ANTHROPIC_API_KEY
 fi
 
-if test -f "$this_dir/.secrets/google-credentials.json"; then
-  GOOGLE_APPLICATION_CREDENTIALS="$this_dir/.secrets/google-credentials.json"
-  export GOOGLE_APPLICATION_CREDENTIALS
-fi
-
 if test -f "$this_dir/.secrets/tavily-api-key.txt"; then
   TAVILY_API_KEY="$(cat "$this_dir/.secrets/tavily-api-key.txt")"
   export TAVILY_API_KEY
 fi
-
-# export LANGFUSE_BASEURL="http://localhost:3000"
-# export LANGFUSE_SECRET_KEY="$(cat "$this_dir/.secrets/langfuse-secret-key.txt")"
-# export LANGFUSE_PUBLIC_KEY="$(cat "$this_dir/.secrets/langfuse-public-key.txt")"
 
 exec "$@"
