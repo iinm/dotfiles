0a0e9896ef21a7218c97e7e03cbfd32e6ba98027
Shumpei IINUMA <shumpei.iinuma@gmail.com>
2025-03-17 21:58:24 +0900

    refactor(agent): move config

---
+++ agent/src/config.mjs
@@ -0,0 +1,69 @@
+/**
+ * @import { ToolUsePattern } from "./tool";
+ */
+
+import { execCommandTool } from "./tools/execCommand.mjs";
+import { tavilySearchTool } from "./tools/tavilySearch.mjs";
+import { tmuxCommandTool } from "./tools/tmuxCommand.mjs";
+
+export const AGENT_MODEL = process.env.AGENT_MODEL || "gpt-4o-mini";
+
+/**
+ * @typedef {object} CreateAllowedToolUsePatternsInput
+ * @property {string} sessionId
+ */
+
+/**
+ * @param {CreateAllowedToolUsePatternsInput} input
+ * @returns {ToolUsePattern[]}
+ */
+export function createAllowedToolUsePatterns({ sessionId }) {
+  /** @type {ToolUsePattern[]} */
+  return [
+    // Web search
+    { toolName: tavilySearchTool.def.name, input: { query: /./ } },
+
+    // Exec command
+    {
+      toolName: execCommandTool.def.name,
+      input: { command: /^(ls|wc|cat|head|tail|fd|rg|find|grep|date)$/ },
+    },
+    {
+      toolName: execCommandTool.def.name,
+      input: { command: "sed", input: { args: ["-n", /^.+p$/] } },
+    },
+    {
+      toolName: execCommandTool.def.name,
+      input: { command: "git", input: { args: [/^(status|diff|log)$/] } },
+    },
+    {
+      toolName: execCommandTool.def.name,
+      input: {
+        command: "git",
+        input: { args: ["branch", "--show-current"] },
+      },
+    },
+    {
+      toolName: execCommandTool.def.name,
+      input: {
+        command: "mkdir",
+        input: { args: ["-p", ".agent/memory"] },
+      },
+    },
+
+    // Tmux command
+    {
+      toolName: tmuxCommandTool.def.name,
+      input: {
+        command: /^(list-sessions|list-windows|capture-pane)$/,
+      },
+    },
+    {
+      toolName: tmuxCommandTool.def.name,
+      input: {
+        command: /^(new-session|new)$/,
+        args: ["-d", "-s", `agent-${sessionId}`],
+      },
+    },
+  ];
+}
--- agent/src/index.mjs
+++ agent/src/index.mjs
@@ -4,6 +4,7 @@
 
 import { createAgent } from "./agent.mjs";
 import { startCLI } from "./cli.mjs";
+import { AGENT_MODEL, createAllowedToolUsePatterns } from "./config.mjs";
 import { createModelCaller } from "./model.mjs";
 import { createPrompt } from "./prompt.mjs";
 import { createToolUseApprover } from "./tool.mjs";
@@ -12,8 +13,6 @@ import { patchFileTool } from "./tools/patchFile.mjs";
 import { tavilySearchTool } from "./tools/tavilySearch.mjs";
 import { tmuxCommandTool } from "./tools/tmuxCommand.mjs";
 import { writeFileTool } from "./tools/writeFile.mjs";
-
-const AGENT_MODEL = process.env.AGENT_MODEL || "gpt-4o-mini";
 
 (async () => {
   // e.g. 2025-12-31-2359
@@ -58,63 +57,3 @@ const AGENT_MODEL = process.env.AGENT_MODEL || "gpt-4o-mini";
   console.error(err);
   process.exit(1);
 });
-
-/**
- * @typedef {object} CreateAllowedToolUsePatternsInput
- * @property {string} sessionId
- */
-
-/**
- * @param {CreateAllowedToolUsePatternsInput} input
- * @returns {ToolUsePattern[]}
- */
-function createAllowedToolUsePatterns({ sessionId }) {
-  /** @type {ToolUsePattern[]} */
-  return [
-    // Web search
-    { toolName: tavilySearchTool.def.name, input: { query: /./ } },
-
-    // Exec command
-    {
-      toolName: execCommandTool.def.name,
-      input: { command: /^(ls|wc|cat|head|tail|fd|rg|find|grep|date)$/ },
-    },
-    {
-      toolName: execCommandTool.def.name,
-      input: { command: "sed", input: { args: ["-n", /^.+p$/] } },
-    },
-    {
-      toolName: execCommandTool.def.name,
-      input: { command: "git", input: { args: [/^(status|diff|log)$/] } },
-    },
-    {
-      toolName: execCommandTool.def.name,
-      input: {
-        command: "git",
-        input: { args: ["branch", "--show-current"] },
-      },
-    },
-    {
-      toolName: execCommandTool.def.name,
-      input: {
-        command: "mkdir",
-        input: { args: ["-p", ".agent/memory"] },
-      },
-    },
-
-    // Tmux command
-    {
-      toolName: tmuxCommandTool.def.name,
-      input: {
-        command: /^(list-sessions|list-windows|capture-pane)$/,
-      },
-    },
-    {
-      toolName: tmuxCommandTool.def.name,
-      input: {
-        command: /^(new-sessions|new)$/,
-        args: ["-d", "-s", `agent-${sessionId}`],
-      },
-    },
-  ];
-}
