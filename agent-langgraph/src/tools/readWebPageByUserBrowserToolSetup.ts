38b6e348f921214d15d4f3ff1a27950895d8454f
Shumpei IINUMA <shumpei.iinuma@gmail.com>
2025-03-17 22:25:56 +0900

    chore: remove old agent implementation

--- agent-langgraph/src/tools/readWebPageByUserBrowserToolSetup.ts
+++
@@ -1,9 +0,0 @@
-import { chromium } from "playwright";
-
-import { USER_DATA_DIR } from "./readWebPageByUserBrowserTool";
-
-(async () => {
-  await chromium.launchPersistentContext(USER_DATA_DIR, {
-    headless: false,
-  });
-})();
