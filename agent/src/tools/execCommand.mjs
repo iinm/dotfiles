e6239ab3687b61fead9da7d83d8f631102aaf3b6
Shumpei IINUMA <shumpei.iinuma@gmail.com>
2025-04-02 23:04:25 +0900

    feat(agent): Ignore errors for rg command with no matches

--- agent/src/tools/execCommand.mjs
+++ agent/src/tools/execCommand.mjs
@@ -92,13 +92,20 @@ Examples:
                 : "<stderr></stderr>",
             ];
             if (err) {
-              // err.message が長過ぎる場合は先頭を表示
-              const errMessageOmitted = err.message.slice(0, OUTPUT_MAX_LENGTH);
-              const isErrMessageOmitted =
-                err.message.length > OUTPUT_MAX_LENGTH;
-              result.push(
-                `\n<error>\n${err.name}: ${errMessageOmitted}${isErrMessageOmitted ? "... (Message omitted)" : ""}</error>`,
-              );
+              // rg: 何もマッチしない場合は exit status != 0 になるので無視
+              const ignoreError = ["rg"].includes(command);
+              if (!ignoreError) {
+                // err.message が長過ぎる場合は先頭を表示
+                const errMessageOmitted = err.message.slice(
+                  0,
+                  OUTPUT_MAX_LENGTH,
+                );
+                const isErrMessageOmitted =
+                  err.message.length > OUTPUT_MAX_LENGTH;
+                result.push(
+                  `\n<error>\n${err.name}: ${errMessageOmitted}${isErrMessageOmitted ? "... (Message omitted)" : ""}</error>`,
+                );
+              }
             }
             return resolve(result.join("\n"));
           },
