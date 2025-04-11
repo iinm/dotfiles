de787d3e91dedad61f1745a108093b6479fd14e7
Shumpei IINUMA <shumpei.iinuma@gmail.com>
2025-04-11 12:20:21 +0900

    chore(nvim): update snippet config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -706,7 +706,32 @@ local setup_cmp = function()
     mapping = cmp.mapping.preset.insert({
       ['<C-p>'] = cmp.mapping.select_prev_item(),
       ['<C-n>'] = cmp.mapping.select_next_item(),
-      ['<CR>'] = cmp.mapping.confirm({ select = false })
+      -- ['<CR>'] = cmp.mapping.confirm({ select = false })
+      --
+      -- https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#super-tab-like-mapping
+      ['<CR>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          if luasnip.expandable() then
+            luasnip.expand()
+          else
+            cmp.confirm({
+              select = true,
+            })
+          end
+        else
+          fallback()
+        end
+      end),
+
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif luasnip.locally_jumpable(1) then
+          luasnip.jump(1)
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
     }),
 
     sources = cmp.config.sources({
@@ -820,6 +845,7 @@ local setup_others = function()
   require('nvim-autopairs').setup()
   require("nvim-surround").setup()
   require('dressing').setup()
+  require('snippets')
 end
 
 setup_options()
