a215a869609db78f723b7798e619dea89a6907f7
Shumpei IINUMA <shumpei.iinuma@gmail.com>
2025-04-13 21:27:08 +0900

    fix(nvim): treesitter folding

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -427,12 +427,12 @@ local setup_auto_commands = function()
   vim.api.nvim_create_autocmd({ 'FileType' }, {
     callback = function()
       if require('nvim-treesitter.parsers').has_parser() then
-        vim.opt.foldmethod = 'expr'
-        vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+        vim.wo.foldmethod = 'expr'
+        vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
       else
-        vim.opt.foldmethod = "indent"
+        vim.wo.foldmethod = 'indent'
       end
-    end,
+    end
   })
 
   -- indent
