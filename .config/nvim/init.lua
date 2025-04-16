f7345a9fe5dbcd63feef457145977823b5c2433c
Shumpei IINUMA <shumpei.iinuma@gmail.com>
2025-04-16 23:35:02 +0900

    chore(nvim): update snippet keymap

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -720,32 +720,32 @@ local setup_cmp = function()
     mapping = cmp.mapping.preset.insert({
       ['<C-p>'] = cmp.mapping.select_prev_item(),
       ['<C-n>'] = cmp.mapping.select_next_item(),
-      -- ['<CR>'] = cmp.mapping.confirm({ select = false })
-      --
-      -- https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#super-tab-like-mapping
-      ['<CR>'] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          if luasnip.expandable() then
-            luasnip.expand()
-          else
-            cmp.confirm({
-              select = true,
-            })
-          end
-        else
-          fallback()
-        end
-      end),
+      ['<CR>'] = cmp.mapping.confirm({ select = false })
 
-      ["<Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_next_item()
-        elseif luasnip.locally_jumpable(1) then
-          luasnip.jump(1)
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
+      -- https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#super-tab-like-mapping
+      -- ['<CR>'] = cmp.mapping(function(fallback)
+      --   if cmp.visible() then
+      --     if luasnip.expandable() then
+      --       luasnip.expand()
+      --     else
+      --       cmp.confirm({
+      --         select = true,
+      --       })
+      --     end
+      --   else
+      --     fallback()
+      --   end
+      -- end),
+      --
+      -- ["<Tab>"] = cmp.mapping(function(fallback)
+      --   if cmp.visible() then
+      --     cmp.select_next_item()
+      --   elseif luasnip.locally_jumpable(1) then
+      --     luasnip.jump(1)
+      --   else
+      --     fallback()
+      --   end
+      -- end, { "i", "s" }),
     }),
 
     sources = cmp.config.sources({
