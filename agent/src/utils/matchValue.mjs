9ba1d4508243d37e6026143b8afbb350c234ccc4
Shumpei IINUMA <shumpei.iinuma@gmail.com>
2025-07-05 21:04:57 +0900

    refactor(agent): simplify matchValue

--- agent/src/utils/matchValue.mjs
+++ agent/src/utils/matchValue.mjs
@@ -18,30 +18,19 @@ export function matchValue(value, pattern) {
     return pattern(value);
   }
   if (Array.isArray(pattern)) {
-    if (Array.isArray(value)) {
-      for (let i = 0; i < pattern.length; i++) {
-        if (!matchValue(value[i], pattern[i])) {
-          return false;
-        }
-      }
-      return true;
-    }
-    return false;
+    return (
+      Array.isArray(value) && pattern.every((p, i) => matchValue(value[i], p))
+    );
   }
   if (typeof pattern === "object") {
-    if (typeof value === "object" && value !== null) {
-      for (const key of Object.keys(pattern)) {
-        if (
-          !matchValue(value[/** @type {keyof value} */ (key)], pattern[key])
-        ) {
-          return false;
-        }
-      }
-      return true;
-    }
-  } else {
-    // Nothing to do
+    return (
+      typeof value === "object" &&
+      value !== null &&
+      Object.entries(pattern).every(([k, p]) =>
+        matchValue(p, value[/** @type {keyof value} */ (k)]),
+      )
+    );
   }
 
-  return true;
+  return false;
 }
