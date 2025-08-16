5ecc7af5bbe997cf324a21cf0e5c989e7b59391a
Shumpei IINUMA <shumpei.iinuma@gmail.com>
2025-08-16 14:05:10 +0900

    fix(nvim): disable blink snippet_forward keymap

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -677,8 +677,24 @@ end
 local setup_blink_cmp = function()
   require('blink.cmp').setup({
     keymap = {
-      preset = 'default',
+      preset = 'none',
       ['<CR>'] = { 'accept', 'fallback' },
+
+      ['<C-e>'] = { 'hide' },
+      ['<C-y>'] = { 'select_and_accept' },
+
+      ['<Up>'] = { 'select_prev', 'fallback' },
+      ['<Down>'] = { 'select_next', 'fallback' },
+      ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
+      ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
+
+      ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+      ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+
+      ['<Tab>'] = { 'snippet_forward', 'fallback' },
+      ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+
+      ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
     },
     completion = {
       list = {
